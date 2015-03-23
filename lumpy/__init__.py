138860dcfde3b0a51724c8cbfb4c7f9425909556
Jorge Barata González <jorge.barata.gonzalez@gmail.com>
2015-03-23 22:26:58 +0000

    Bump version: 0.1.0 → 0.1.1

--- lumpy/__init__.py
+++ lumpy/__init__.py
@@ -2,7 +2,7 @@ from .lumpy import App, Mail
 
 __author__ = "Jorge Barata"
 __author_email__ = "jorge.barata.gonzalez@gmail.com"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __license__ = "MIT"
 __doc__ = Mail.send.__doc__
 
