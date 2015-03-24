20391ce9a645e90c0f50605a3c706efe9eb24738
Jorge Barata González <jorge.barata.gonzalez@gmail.com>
2015-03-24 17:22:55 +0000

    fix main

--- lumpy/__init__.py
+++ lumpy/__init__.py
@@ -7,7 +7,3 @@ __author__ = "Jorge Barata"
 __author_email__ = "jorge.barata.gonzalez@gmail.com"
 __version__ = "0.1.1"
 __license__ = "MIT"
-
-
-if __name__ == "__main__":
-    App().run()
---
+++ lumpy/__main__.py
@@ -0,0 +1,3 @@
+from .lumpy import App
+
+App().run()
